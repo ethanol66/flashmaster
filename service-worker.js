0cda252bdcf0c8309b997bb2be30a249a8c7e7e1
ethanol66 <pearseth002@gmail.com>
2026-06-23 21:01:27 -0600

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const CACHE='flashmaster-cache-v1';const FILES=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
